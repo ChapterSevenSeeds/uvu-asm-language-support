e74d5b0e732c3e3d76267fec0071a491b48c6ac1
Tyson Jones <ballisticmissle@gmail.com>
2022-09-13 21:42:48 -0600

    Trying to publish next version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to the "uvu-asm-language-support" extension will be document
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+## [0.0.1] - 9/13/2022
 
-- Initial release
+- Initial release with syntax highlighting
+
+## [0.0.2] - 9/13/2022
+
+- Added formatting
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,37 @@
-import { HoverProvider, ProviderResult, Hover, languages,  } from 'vscode';
+import { HoverProvider, ProviderResult, Hover, languages, Range, TextEdit, Position } from 'vscode';
 
 languages.registerDocumentFormattingEditProvider('uvuasm', {
     provideDocumentFormattingEdits(document, options, token) {
+        const edits: TextEdit[] = [];
 
+        const text = document.getText().split('\n');
+
+        let maxLabelLength = 1;
+        for (const line of text) {
+            const match = /^\w+/.exec(line)?.[0];
+            if (match != null && match.length > maxLabelLength) maxLabelLength = match.length;
+        }
+
+        const insertChar = options.insertSpaces ? ' ' : '\t';
+        let insertCount = Math.ceil(maxLabelLength / options.tabSize) * (options.insertSpaces ? options.tabSize : 1);
+        if (insertCount === maxLabelLength / (options.insertSpaces ? 1 : options.tabSize)) insertCount += options.insertSpaces ? options.tabSize : 1;
+
+        for (const [index, line] of Object.entries(text)) {
+            let whiteSpaceStart = -1, whiteSpaceEnd = -1;
+            for (let i = 0; i < line.length; ++i) {
+                if (/\w/.test(line[i])) {
+                    if (whiteSpaceStart != -1) {
+                        whiteSpaceEnd = i;
+                        break;
+                    } 
+                } else if (whiteSpaceStart == -1)  {
+                    whiteSpaceStart = i;
+                }
+            }
+
+            if (whiteSpaceEnd !== -1 && whiteSpaceStart !== -1) edits.push(TextEdit.replace(new Range(new Position(+index, whiteSpaceStart), new Position(+index, whiteSpaceEnd)), "".padStart(insertCount - (options.insertSpaces ? whiteSpaceStart : Math.floor(whiteSpaceStart / options.tabSize)), insertChar)));
+        }
+
+        return edits;
     }
 })
