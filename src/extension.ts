27abddaba7ade861a40cbe1c0d287a28d7856888
Tyson Jones <ballisticmissle@gmail.com>
2022-09-14 09:34:16 -0600

    Need to fix formatting with tabs

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,44 @@
-import { HoverProvider, ProviderResult, Hover, languages, Range, TextEdit, Position } from 'vscode';
+import { HoverProvider, ProviderResult, Hover, languages, Range, TextEdit, Position, DebugConsoleMode } from 'vscode';
 
 languages.registerDocumentFormattingEditProvider('uvuasm', {
     provideDocumentFormattingEdits(document, options, token) {
-        const edits: TextEdit[] = [];
+        function lengthInSpaces(length: number) {
+            return length;
+        }
+        function lengthInTabs(length: number) {
+            return Math.ceil(length / options.tabSize);
+        }
 
+        const replacementChar = options.insertSpaces ? ' ' : '\t';
+        const lengthConversionFunction = options.insertSpaces ? lengthInSpaces : lengthInTabs;
         const text = document.getText().split('\n');
 
-        let maxLabelLength = 1;
-        for (const line of text) {
-            const match = /^\w+/.exec(line)?.[0];
-            if (match != null && match.length > maxLabelLength) maxLabelLength = match.length;
-        }
-
-        const insertChar = options.insertSpaces ? ' ' : '\t';
-        let insertCount = Math.ceil(maxLabelLength / options.tabSize) * (options.insertSpaces ? options.tabSize : 1);
-        if (insertCount === maxLabelLength / (options.insertSpaces ? 1 : options.tabSize)) insertCount += options.insertSpaces ? options.tabSize : 1;
+        let maxLabelLength = 1; // In spaces
+        const whiteSpaceRanges: {
+            range: Range,
+            labelLength: number
+        }[] = [];
 
         for (const [index, line] of Object.entries(text)) {
-            let whiteSpaceStart = -1, whiteSpaceEnd = -1;
-            for (let i = 0; i < line.length; ++i) {
-                if (/\w/.test(line[i])) {
-                    if (whiteSpaceStart != -1) {
-                        whiteSpaceEnd = i;
-                        break;
-                    } 
-                } else if (whiteSpaceStart == -1)  {
-                    whiteSpaceStart = i;
-                }
-            }
+            if (!Boolean(line.trim()) || line.trim().startsWith(';')) continue;
 
-            if (whiteSpaceEnd !== -1 && whiteSpaceStart !== -1) edits.push(TextEdit.replace(new Range(new Position(+index, whiteSpaceStart), new Position(+index, whiteSpaceEnd)), "".padStart(insertCount - (options.insertSpaces ? whiteSpaceStart : Math.floor(whiteSpaceStart / options.tabSize)), insertChar)));
+            const match = /^(\w*)(\s+)/.exec(line);
+
+            if (match != null) {
+                if (match[1].length > maxLabelLength) {
+                    maxLabelLength = match[1].length;
+                }
+
+                whiteSpaceRanges.push({
+                    range: new Range(new Position(Number(index), match[1].length), new Position(Number(index), match[1].length + match[2].length)),
+                    labelLength: match[1].length // In spaces
+                });
+            }
         }
 
-        return edits;
+        let insertChars = Math.floor(lengthConversionFunction(maxLabelLength) / options.tabSize) * options.tabSize;
+        if (insertChars % options.tabSize === 0) insertChars += options.insertSpaces ? options.tabSize : 1;
+
+        return whiteSpaceRanges.map(x => new TextEdit(x.range, "".padStart(insertChars - lengthConversionFunction(x.labelLength), replacementChar)))
     }
 })
